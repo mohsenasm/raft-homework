78973c29c6eb6b7b4600cc624ecdf4e7aecec838
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2023-11-10 16:56:59 +0330

    add consensus.py

---
+++ consensus.py
@@ -0,0 +1,14 @@
+def initialize(init_value: int, my_port: int, other_ports: [int]):
+    pass
+
+def get() -> int:
+    pass
+
+def add(value: int):
+    pass
+
+def sub(value: int):
+    pass
+
+def mul(value: int):
+    pass
